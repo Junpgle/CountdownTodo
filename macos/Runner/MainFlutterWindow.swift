b4b209fa432522b78380479a74e5c2dc9658cda7
Junpgle <81464408+Junpgle@users.noreply.github.com>
2026-06-11 19:59:25 +0800

    v4.15 【修复】壁纸渐显效果回归 【新增】Mac支持开机自启

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,6 +1,7 @@
 import Cocoa
 import FlutterMacOS
 import WidgetKit
+import LaunchAtLogin
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
@@ -73,6 +74,25 @@ class MainFlutterWindow: NSWindow {
         }
         result(true)
       } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
+    // Launch at startup channel
+    let launchAtStartupChannel = FlutterMethodChannel(
+      name: "launch_at_startup",
+      binaryMessenger: flutterViewController.engine.binaryMessenger
+    )
+    launchAtStartupChannel.setMethodCallHandler { (call, result) in
+      switch call.method {
+      case "launchAtStartupIsEnabled":
+        result(LaunchAtLogin.isEnabled)
+      case "launchAtStartupSetEnabled":
+        if let arguments = call.arguments as? [String: Any] {
+          LaunchAtLogin.isEnabled = arguments["setEnabledValue"] as! Bool
+        }
+        result(nil)
+      default:
         result(FlutterMethodNotImplemented)
       }
     }
